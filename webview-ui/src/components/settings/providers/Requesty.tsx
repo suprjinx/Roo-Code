f154eb2ebfbb6921a664adfbf2fae3eebb68add3
Geoff Wilson <geoff@gr-oss.io>
2025-07-25 13:45:29 -0400

    One more lint fix
    Signed-off-by: Geoff Wilson <geoff@gr-oss.io>

--- webview-ui/src/components/settings/providers/Requesty.tsx
+++ webview-ui/src/components/settings/providers/Requesty.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react"
+import { useState } from "react"
 
 import { type ProviderSettings, type OrganizationAllowList, requestyDefaultModelId } from "@roo-code/types"
 
